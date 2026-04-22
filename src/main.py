4e3f8df47b8b1bd3d45aa4761c39f861e577a15f
affection7 <salkow12345@gmail.com>
2026-04-22 15:35:43 +0300

    MWP

--- src/main.py
+++ src/main.py
@@ -5,9 +5,6 @@ from load import load_to
 
 URL = "https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum,solana&names=Bitcoin&symbols=btc"
 if __name__ == "__main__":
-    try:
-        data = extract_price(URL)
-        data_df = transform_price(data)
-        load_to(data_df)
-    except:
-        print("ERROR")
+    data = extract_price(URL)
+    data_df = transform_price(data)
+    load_to(data_df)
