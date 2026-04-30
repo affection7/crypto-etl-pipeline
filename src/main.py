9a594d97e9a631984901c230b0d3c074c04e31cf
affection7 <salkow12345@gmail.com>
2026-04-30 10:06:33 +0300

    refactor: add logs in main

--- src/main.py
+++ src/main.py
@@ -5,7 +5,10 @@ from load import load_to
 
 URL = "https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum,solana&names=Bitcoin&symbols=btc"
 if __name__ == "__main__":
+    print("[EXTRACT] start")
     data = extract_price(URL)
+    print("[TRANSFORM] start")
     data_df = transform_price(data)
+    print("[LOAD] start")
     load_to(data_df)
     print("[PIPELINE] done")
--- src/transform.py
+++ src/transform.py
@@ -5,7 +5,6 @@ def transform_price(data: dict):
     rows = []
     date = datetime.now(timezone.utc)
     
-    print("[TRANSFORM] start")
     for coin, price in data.items():
         rows.append({"date": date, "price": price['usd'], "coin": coin})
     print(f'[TRANSFORM] processed {len(data.items())} coins')
