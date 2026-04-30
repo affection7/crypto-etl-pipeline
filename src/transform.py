f84028a552bc4e1484eefac594633be7e3bf4354
affection7 <salkow12345@gmail.com>
2026-04-30 19:25:46 +0300

    feat: add support for various API response structures

--- src/transform.py
+++ src/transform.py
@@ -1,11 +1,25 @@
 import pandas as pd
 from datetime import datetime, timezone
 
-def transform_price(data: dict):
+def transform_price(data_list: list):
     rows = []
     date = datetime.now(timezone.utc)
     
-    for coin, price in data.items():
-        rows.append({"date": date, "price": price['usd'], "coin": coin})
-    print(f'[TRANSFORM] processed {len(data.items())} coins')
+    # Итерируемся по списку словарей
+    for item in data_list:
+        if "quotes" in item:
+            rows.append({
+                "date": date, 
+                "price": item["quotes"]["USD"]["price"], 
+                "coin": item["symbol"]
+            })
+        else:
+            for coin, price in item.items():
+                rows.append({
+                    "date": date, 
+                    "price": price['usd'],
+                    "coin": coin
+                })
+    
+    print(f'[TRANSFORM] processed {len(rows)} coins')
     return pd.DataFrame(rows)
