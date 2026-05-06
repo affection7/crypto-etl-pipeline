124261bedf9fbcb485c2c21724d865944549f196
affection7 <salkow12345@gmail.com>
2026-05-06 10:22:50 +0300

    refactor: replace print statements with logging

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 
 from dotenv import load_dotenv
 from requests.exceptions import HTTPError
@@ -8,24 +9,24 @@ load_dotenv()
 def extract_price(url_gecko: str, headers_gecko: dict, url_paprika: str):
     data = {}
     try:
-        response_bes = requests.get(url_gecko, headers=headers_gecko)
+        response_bes = requests.get(url_gecko, headers=headers_gecko, timeout=10)
         response_bes.raise_for_status()
         data_bes = response_bes.json()
         data["gecko"] = data_bes 
-        print("[EXTRACT] gecko success")
+        logging.info("[EXTRACT] gecko success")
     except HTTPError as http_gecko_error:
-        print(f"HTTP ощибка: {http_gecko_error}")
+        logging.error(f"HTTP ощибка: {http_gecko_error}")
         
     try:
-        response_ton = requests.get(url_paprika)
+        response_ton = requests.get(url_paprika, timeout=10)
         response_ton.raise_for_status()
         data_ton = response_ton.json()
         data["paprika"] = data_ton
-        print("[EXTRACT] paprika success")
+        logging.info("[EXTRACT] paprika success")
     except HTTPError as http_paprika_error:
-        print(f"HTTP ощибка: {http_paprika_error}")
+        logging.error(f"HTTP ощибка: {http_paprika_error}")
         
     if len(data) == 0:
-        print("FATAL ERROR")
-    print(f"[EXTRACT] done. Collected {len(data)} items")
+        logging.error("FATAL ERROR")
+    logging.info(f"[EXTRACT] done. Collected {len(data)} items")
     return data
