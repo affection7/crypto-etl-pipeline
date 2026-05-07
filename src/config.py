eff7df99c7ee507f934f128e66b1ca8349ed4904
affection7 <salkow12345@gmail.com>
2026-05-07 16:07:49 +0300

    feat: replace url connecting database

--- src/config.py
+++ src/config.py
@@ -8,4 +8,4 @@ HEADERS = {"x-cg-api-key": API_KEY}
 URL_GECKO  = os.getenv("URL_GECKO")
 URL_PAPRIKA = os.getenv("URL_PAPRIKA")
 DB_PASS = os.getenv("DB_PASS")
-DB_URL = f"postgresql://neondb_owner:{DB_PASS}@ep-shiny-firefly-ah7am9sh-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&connect_timeout=10"
+DATABASE_URL = os.getenv("DATABASE_URL")
