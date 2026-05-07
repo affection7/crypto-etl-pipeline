929f797955b20e6de9bdb6871e5d67c104d12422
affection7 <salkow12345@gmail.com>
2026-05-07 12:15:34 +0300

    refactor: add import headers

--- src/config.py
+++ src/config.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+HEADERS = {"x-cg-api-key": API_KEY}
 URL_GECKO  = os.getenv("URL_GECKO")
 URL_PAPRIKA = os.getenv("URL_PAPRIKA")
 DB_PASS = os.getenv("DB_PASS")
+DB_URL = f"postgresql://neondb_owner:{DB_PASS}@ep-shiny-firefly-ah7am9sh-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&connect_timeout=10"
