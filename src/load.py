390ef22ec8e22d3a2f216ba5189ea9a1ca11b87d
affection7 <salkow12345@gmail.com>
2026-05-07 16:08:02 +0300

    feat: replace url connecting database

--- src/load.py
+++ src/load.py
@@ -1,20 +1,16 @@
 import pandas as pd
 import psycopg2
 import logging
-from config import DB_URL
+from config import DATABASE_URL
 from psycopg2.extras import execute_values
 
 def load_to(df: pd.DataFrame):
     with open(file='sql/insert_coins.sql') as f_sql:
         sql = f_sql.read()
 
-    if df.empty:
-        logging.info("[LOAD] No rows to load!!!")
-        return
-
     rows = df[["date", "price", "coin", "source"]].values.tolist()
 
-    with psycopg2.connect(DB_URL) as conn:
+    with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cur:
             execute_values(cur, sql, rows)
             logging.info(f'[LOAD] rows prepared: {len(rows)}')
