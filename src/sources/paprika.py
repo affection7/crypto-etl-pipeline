8b6e60ee601d10b68f956cc66aa730baad5acaa4
affection7 <salkow12345@gmail.com>
2026-05-12 15:41:49 +0300

    refactor: clean up paprika source extraction

--- src/sources/paprika.py
+++ src/sources/paprika.py
@@ -1,14 +1,8 @@
-
 import requests
 import logging
 from requests.exceptions import HTTPError
-import pandas as pd
-import logging
-from datetime import datetime, timezone
 
-URL = "https://api.coinpaprika.com/v1/tickers/ton-toncoin"
-
-def extract_paprika(url: str) -> list[dict]:
+def extract_paprika(url: str) -> dict:
     try:
         response = requests.get(url, timeout=10)
         response.raise_for_status()
@@ -18,19 +12,4 @@ def extract_paprika(url: str) -> list[dict]:
     except HTTPError as http_paprika_error:
         logging.error(f"HTTP ощибка: {http_paprika_error}")
 
-def transform_price(data_list: dict):
-    rows = []
-    date = datetime.now(timezone.utc)
 
-    rows.append({
-        "date": date,
-        "price": round(float(data_list["quotes"]["USD"]["price"]),3),
-        "coin": data_list["name"].lower(),
-        "source": "paprika",
-
-    })
-
-    print(f'[TRANSFORM] processed {len(rows)} coins {rows}')
-    return pd.DataFrame(rows)
-
-transform_price(extract_paprika(URL))
