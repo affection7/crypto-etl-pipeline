459159861eb1f4f17d254814ec451f01d8cd7533
affection7 <salkow12345@gmail.com>
2026-05-13 13:01:11 +0300

    refactor: make loader accept sql path and selected columns

--- src/loaders/load.py
+++ src/loaders/load.py
@@ -4,11 +4,11 @@ import logging
 from config import DATABASE_URL
 from psycopg2.extras import execute_values
 
-def load_to(df: pd.DataFrame):
-    with open(file='sql/insert_coins.sql') as f_sql:
+def load_to(df: pd.DataFrame, sql_path: str, columns: list[str]):
+    with open(file=sql_path) as f_sql:
         sql = f_sql.read()
 
-    rows = df[["date", "price", "coin", "source"]].values.tolist()
+    rows = df[columns].values.tolist()
 
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cur:
